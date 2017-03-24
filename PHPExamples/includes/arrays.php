c06b145ea0e609a18f61cd89158c1b667d98fe36
Hai Phan <htp200006@gmail.com>
2017-03-24 05:28:15 -0500

    Added webpages using array in arrays.php and changed nav to pull from arrays.php in foreach loop

---
+++ PHPExamples/includes/arrays.php
@@ -0,0 +1,23 @@
+<?php
+	//Nav menu items
+	$navItems = array(
+					//Pages are here
+					array(
+						"page" => "index.php",
+						"title" => "Home"
+					),
+					array(
+						"page" => "course.php",
+						"title" => "Course"
+					),
+					array(
+						"page" => "book.php",
+						"title" => "Book"
+					),
+					array(
+						"page" => "contact.php",
+						"title" => "Contact"
+					)
+					//add more pages below
+				);
+?>
--- PHPExamples/includes/nav.php
+++ PHPExamples/includes/nav.php
@@ -1,6 +1,7 @@
 <ul>
-	<li><a href="index.php">Home</a></li>
-	<li><a href="team.php">Team</a></li>
-	<li><a href="menu.php">Menu</a></li>
-	<li><a href="contact.php">Contact</a></li>
+	<?php
+		foreach ($navItems as $item){
+			echo "<li><a href=\"$item[page]\">$item[title]</a></li>";
+		}
+	?>
 </ul>
